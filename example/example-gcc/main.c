d556b4c70ef658e274ef0c1b2001bb0c3b7cbe9a
Paulo Matias <paulo.matias@usp.br>
2013-10-14 10:32:36 -0300

    Fix ident

--- example/example-gcc/main.c
+++ example/example-gcc/main.c
@@ -10,9 +10,9 @@ static void HID_bWrite(void) {
 	// Calls to mikroC functions need to be enclosed
 	// with these pushs and pops because of the
 	// different calling convention.
-    asm("push {r4, r5, r6, r7, r8, r9, r10, r11}");
-    while(!HID_Write((char*)writebuff, HIDsize));
-    asm("pop {r4, r5, r6, r7, r8, r9, r10, r11}");
+	asm("push {r4, r5, r6, r7, r8, r9, r10, r11}");
+	while(!HID_Write((char*)writebuff, HIDsize));
+	asm("pop {r4, r5, r6, r7, r8, r9, r10, r11}");
 }
 
 uint8_t do_calculation(uint8_t a, uint8_t b) {
